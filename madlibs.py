c023325d004f302ef5d6282ce268222990794349
Shokhrukh123 <sonicjkee@gmail.com>
2019-09-03 23:18:50 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+Occupation = input("Occupation: ")
+noun = input("noun: ")
+Adjective = input("Adjective: ")
+Noun = input("Noun1: ")
+Verb = input("Verb1: ")
+Adjective = input("Adjective1: ")
+Noun = input("Noun: ")
+Verb = input("Verb: ")
+Noun = input("Noun: ")
+Verb = input("Verb: ")
+
+
+
+
+
+
+print("Today a Occupation named" + Noun + "came to our school to talk to us about her job." +
+"She said the most" + "important skill you need to know to do her job is to be able to " +
+Verb + "around" + Adjective  + Noun + ". She said it was easy for her to learn her job" +
+"because she loved to" + Verb + "when she was my age and that helps a lot! If youre"
++ "considering her profession, I hope you can be near" + Adjective + noun + ". That is" +
+"very important! If you get too distracted in that situation you won't be able to " +
+Verb +  "next to (a)" + Noun +"!")
